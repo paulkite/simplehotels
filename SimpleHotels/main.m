4b3765dc6f9af96905cc62ea20272dd567dc9c95
Sebastian Celis <scelis@mobiata.com>
2013-02-27 14:24:08 -0600

    Add initial project.

---
+++ SimpleHotels/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleHotels
+//
+//  Created by Sebastian Celis on 2/27/13.
+//  Copyright (c) 2013 Mobiata, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SHAppDelegate class]));
+    }
+}
